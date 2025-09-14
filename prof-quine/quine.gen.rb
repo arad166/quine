d96619e3a605d8be856f28447335a17ae33aaaa3
arad166 <1a88.19e7@gmail.com>
2025-09-14 23:33:33 +0900

    update prof-quine

--- prof-quine/quine.gen.rb
+++ prof-quine/quine.gen.rb
@@ -1,7 +1,7 @@
 eval$s=%w(
 s=%(eval$s=%w(#{$s})*"");
-func=->n{s.slice!(0,n)};
-fish="#33H5#32@#22A26#22@#17R36#17@#13A44#13@#10D50#10@#8A14#12H28#8@#6A12#16R30#6@#5A10#15D35#5@#3A10#9H45#3@#2A10#11R45#2@#2A9#15D42#2@#A10#20H38#@#A11#24R33#@#A11#29D28#@#A13#33H22#@#A15#35R18#@#2A17#36D13#2@#3A20#35H10#2@#4A24#31R8#3@#5A32#22D6#5@#6A41#9H8#6@#8A54#8@#11R49#10@#13A44#13@#17D36#17@#22A26#22@#34H2#34";
+f=->n{s.slice!(0,n)};
+fish="#70@#23H23#24@#18A34#18@#14R12#8A22#14@#11D12#11A25#11@#9H12#12A28#9@#7R12#8A36#7@#5D13#6A41#5@#4H13#9A40#4@#3R14#11A39#3@#2D14#15A37#2@#2H15#18A33#2@#R16#22A30#@#D17#24A27#@#H19#26A23#@#2R20#27A19#2@#2D22#27A17#2@#3H24#25A15#3@#4R29#20A13#4@#5D33#14A13#5@#6H57#7@#8A54#8@#11R48#11@#13A44#13@#17D36#17@#22A26#22@#70";
 output = "";
 i = 0;
 len=fish.size;
@@ -10,15 +10,15 @@ len.times{
   c = fish[i];
   if(c!='@'&&c!='#');
     j = i + 1;
-    space_len = "";
+    len = "";
     (fish.size - j).times{|k|
       if(!(fish[j] =~ /[0-9]/));
         break;
       end;
-      space_len << fish[j];
+      len << fish[j];
       j += 1;
     };
-    count = space_len.to_i;
+    count = len.to_i;
     print(32.chr * count);
     i = j;
     elsif(c == "@");
@@ -34,10 +34,10 @@ len.times{
       l << fish[j];
       j += 1;
     };
-    let_len = l.to_i;
-    print(func[let_len]);
+    len = l.to_i;
+    print(f[len]);
     i = j;
   end;
 };
-puts();#This_program_is_a_Ruby_quine.______##arad166##Kento_Harada####
+puts();#This_program_is_a_Ruby_quine.___##arad166##Kento_Harada####
 )*""
